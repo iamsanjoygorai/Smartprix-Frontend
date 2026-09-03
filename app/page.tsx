df83d58f4de93326870485f78d8cc40459584eb6
Sanjoy Gorai <iamsanjoygorai@gmail.com>
2026-09-03 21:03:27 +0530

    basic code setup completed

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white">
+      <h1 className="p-10 text-3xl font-bold text-gray-900">
+        Smartprix Clone
+      </h1>
+    </main>
+  );
+}
